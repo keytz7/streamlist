263a43df1f14d6276ea3086d05d7ea0c71002688
keytz7 <keyontudor709@gmail.com>
2025-04-19 13:10:57 -0700

    App.js

--- streamlist/src/App.js
+++ streamlist/src/App.js
@@ -1,23 +1,17 @@
-import React from 'react'; 
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar'; 
+import Navbar from './components/Navbar';
 import StreamList from './components/StreamList';
-import Movies from './components/Movies'; 
-import Cart from './components/Cart'; 
-import About from './components/About'; 
 
 const App = () => {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" exact component={StreamList} />
-        <Route path="/movies" component={Movies} />
-        <Route path="/cart" component={Cart} />
-        <Route path="/about" component={About} />
-      </Routes>
-    </Router>
-  );
-}; 
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<StreamList />} />
+            </Routes>
+        </Router>
+    );
+};
 
-export default App; 
+export default App;
