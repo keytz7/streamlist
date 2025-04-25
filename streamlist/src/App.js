e38384f3c570d5c9c3694829d46abcd131749759
keytz7 <keyontudor709@gmail.com>
2025-04-25 15:29:54 -0700

    App.js

--- streamlist/src/App.js
+++ streamlist/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import StreamList from './components/StreamList';
+import Movies from './components/Movies'; // Importing the new Movies component
 
 const App = () => {
     return (
@@ -9,6 +10,7 @@ const App = () => {
             <Navbar />
             <Routes>
                 <Route path="/" element={<StreamList />} />
+                <Route path="/movies" element={<Movies />} /> {/* New route for movies */}
             </Routes>
         </Router>
     );
