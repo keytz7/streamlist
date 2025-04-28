ea62ba45d63a1d531d2a40da4241394405ae7ff1
keytz7 <keyontudor709@gmail.com>
2025-04-28 15:27:14 -0700

    App.js

--- streamlist/src/App.js
+++ streamlist/src/App.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import StreamList from './components/StreamList';
-import Movies from './components/Movies'; // Importing the new Movies component
+import Movies from './components/Movies';
+import Cart from './components/Cart';
 
 const App = () => {
+    const [cartItems, setCartItems] = useState([]); // Initialize with an empty array
+
+    useEffect(() => {
+        // Load cart items from localStorage on component mount
+        const storedCart = JSON.parse(localStorage.getItem('cartItems')) || [];
+        setCartItems(storedCart); // Set cart items from localStorage
+    }, []);
+
     return (
         <Router>
-            <Navbar />
+            <Navbar cartItemsCount={cartItems.length} />
             <Routes>
-                <Route path="/" element={<StreamList />} />
-                <Route path="/movies" element={<Movies />} /> {/* New route for movies */}
+                <Route path="/" element={<StreamList setCartItems={setCartItems} cartItems={cartItems} />} />
+                <Route path="/movies" element={<Movies />} />
+                <Route path="/cart" element={<Cart cartItems={cartItems} setCartItems={setCartItems} />} />
             </Routes>
         </Router>
     );
