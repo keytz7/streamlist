fd1304b4ceaef9a0b94095d9450805c786136ece
keytz7 <keyontudor709@gmail.com>
2025-05-10 02:08:01 -0700

    service-worker.js

--- streamlist/public/service-worker.js
+++ streamlist/public/service-worker.js
@@ -1,12 +1,29 @@
 const CACHE_NAME = 'eztechmovie-cache-v1';
 const urlsToCache = [
+  Service-worker.js file:
+/* eslint-env serviceworker */
+/* global self */
+
+const CACHE_NAME = 'eztechmovie-cache-v1';
+const urlsToCache = [
     '/',
     '/index.html',
     '/favicon.ico',
     '/logo192.png',
     '/logo512.png',
     '/styles.css',
+    'public/manifest.json',
+    '/robots.txt',
+    '/mainpage.png',
+    '/moviespage.png',
+    '/cartpage.png',
+    // Add bundled JS files here. These paths depend on your build output.
+    'build/static/js/bundle.js',
+    'build/static/js/0.chunk.js',
+    'build/static/js/main.chunk.js',
+
 ];
+
 
 self.addEventListener('install', (event) => {
     event.waitUntil(
